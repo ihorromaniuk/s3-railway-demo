381b495280faf605fcee96e7db5a63f853fac598
Ihor Romaniuk <ihor.rome@gmail.com>
2026-03-22 18:31:59 +0200

    change java version to 21

--- src/main/java/core/basesyntax/s3railwaydemo/S3RailwayDemoApplication.java
+++ src/main/java/core/basesyntax/s3railwaydemo/S3RailwayDemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class S3RailwayDemoApplication {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(S3RailwayDemoApplication.class, args);
     }
 }
